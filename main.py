cd8853ffb5958b11de6fdace4c8067885b6d4ca3
Bekzhan Aktoreyev <bekzhanaktoreyev@MacBook-Pro-Bekzhan.local>
2024-04-17 13:58:44 +0500

    cors

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import uvicorn
 from service import app
 from models import Base
 from utils import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 def start_uvicorn(loop):
     config = uvicorn.Config(app, loop=loop)
@@ -17,6 +18,13 @@ def start_bot(loop):
             
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     start_bot(loop)
